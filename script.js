f87c3088c2034b36d737f3ab2d892016c90324cb
bashir ahammed <bahammed16@gmail.com>
2020-08-06 00:24:42 +0400

    signup and login form

---
+++ script.js
@@ -0,0 +1,60 @@
+var myArr=[];
+document.getElementById('signup-btn').addEventListener('click', ()=>{
+   let fullName=document.getElementById('signup-name').value;
+   var signUpemail=document.getElementById('signup-email').value;
+   var signUppassword=document.getElementById('signup-password').value;
+  
+
+    let signupData={
+        fuullName:fullName,
+        email:signUpemail,
+        password:signUppassword
+    }
+    myArr.push(signupData);
+  
+    console.log(myArr);
+
+           var h1=document.getElementById('h1');
+            h1.innerHTML="👏 Successfully Sign up";
+            h1.style.backgroundColor='green';
+            h1.style.color='#fff';
+            h1.style.padding="5px";
+            h1.style.borderRadius='5px';
+   
+});
+
+
+
+
+document.getElementById('signin-btn').addEventListener('click', ()=>{
+    
+    isDataEqual();
+})
+
+function isDataEqual(){
+    var signEmail=document.getElementById('signin-email').value;
+     var signPassword=document.getElementById('signin-password').value;
+    myArr.forEach((data) =>{
+        if(signEmail==data.email && signPassword == data.password ){
+            var h1=document.getElementById('h1');
+            h1.innerHTML="😍 Successfully login";
+            h1.style.backgroundColor='green';
+            h1.style.color='#fff';
+            h1.style.padding="5px";
+            h1.style.borderRadius='5px';
+            // console.log(signPassword,signEmail + '  ' + 'Successfully login');
+        }else{
+            var h1=document.getElementById('h1');
+            h1.innerHTML= "😢 Sorry try again";
+            h1.style.backgroundColor='grey';
+            
+             h1.style.color='red';
+             h1.style.padding="5px";
+             h1.style.borderRadius='5px';
+            // console.log(signPassword,signEmail + '  ' + 'Not match User');
+        }
+    });
+}
+
+
+// git remote add origin https://github.com/bashirdev/signup-login.git
